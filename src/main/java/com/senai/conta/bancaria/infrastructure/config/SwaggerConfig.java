17ebd0c014b13d500a06163afc06b9138d3ebf9b
pietrarocha <pietrarocha72@gmail.com>
2025-10-29 10:02:16 -0300

    29/10/2025

--- src/main/java/com/senai/conta/bancaria/infrastructure/config/SwaggerConfig.java
+++ src/main/java/com/senai/conta/bancaria/infrastructure/config/SwaggerConfig.java
@@ -2,21 +2,31 @@ package com.senai.conta.bancaria.infrastructure.config;
 
 import io.swagger.v3.oas.models.*;
 import io.swagger.v3.oas.models.info.*;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.*;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public OpenAPI contaOpenAPI() {
+    public OpenAPI oficinaOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("API - Conta Bancária")
-                        .description("Cadastro e gestão de serviços de uma conta bancária.")
+                        .title("API - Conta Bancaria")
+                        .description("Cadastro e gestão de serviços de um banco.")
                         .version("1.0")
                         .contact(new Contact()
-                                .name("Equipe Bancária")
-                                .email("suporte@banco.com"))
-                );
+                                .name("Equipe Banco")
+                                .email("suporte@banco.com")))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()));
+    }
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
     }
 }
